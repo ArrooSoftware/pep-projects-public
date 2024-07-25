fa0615d7ac97147fc2f13d76b991176f5f6ea255
Nockm <nockmss@gmail.com>
2024-07-25 14:47:25 +0100

    feat: Update UI appropriately when busy - Show progress bar - Grey-out input+output - Disable input

--- freq/frequency/index.js
+++ freq/frequency/index.js
@@ -1,5 +1,23 @@
+function setIsBusy(isBusy) {
+	// Get elements.
+	var inputTextArea = document.getElementById("inputTextArea");
+	var outputTextArea = document.getElementById("outputTextArea");
+	var progressBar = document.getElementById("progressBar");
+
+	// Freeze input when busy.
+	inputTextArea.readOnly = isBusy;
+
+	// Grey-out input+output when busy.
+	inputTextArea.style.opacity = isBusy ? 0.2 : 1.0;
+	outputTextArea.style.opacity = isBusy ? 0.2 : 1.0;
+
+	// Show progress bar when busy.
+	progressBar.style.opacity = isBusy ? 1.0 : 0.0;
+}
+
 function convertText(inputText) {
 	var worker = new Worker("worker.js");
+	setIsBusy(true);
 
 	var results;
 
@@ -7,6 +25,7 @@ function convertText(inputText) {
 		results = e.data;
 		console.log(e.data);
 		outputTextArea.value = e.data;
+		setIsBusy(false);
 	});
 
 	worker.postMessage(inputText);
